77796d2d3cdb674b34fa726558db83bece3ab558
Ethan <0xethans@gmail.com>
2025-11-19 11:25:22 -0500

    Updated onfig file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  outputFileTracingIncludes: {
+    '/actions/**': ['./zig/binaries/**/*'],
+  },
 };
 
 export default nextConfig;
